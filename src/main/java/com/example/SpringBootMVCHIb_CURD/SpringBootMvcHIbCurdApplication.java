0104a64b79b6f6d5f3818a652e712b6d56684b71
Vaibhav <Vaibhav@DESKTOP-M3KTQQP>
2020-04-12 19:42:18 +0530

    First Commit

---
+++ src/main/java/com/example/SpringBootMVCHIb_CURD/SpringBootMvcHIbCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMVCHIb_CURD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcHIbCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcHIbCurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootMVCHIb_CURD/SpringBootMvcHIbCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMVCHIb_CURD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcHIbCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
